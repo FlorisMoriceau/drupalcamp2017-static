310e8d61b7fd9e31c053a4b5b07001ff037c8295
Floris MORICEAU <floris.moriceau@niji.fr>
2017-07-10 18:27:37 +0200

    WIP page partners

--- partners.php
+++ partners.php
@@ -14,8 +14,10 @@
     <div class="row ">
       <div class="col-md-12">
         <div class="lead text-center">
-          Le drupalcamp est un évènement proposé par l'association Drupal France et francophonie, par des bénévoles. <br>
-          Nous faisons alors appel, comme tous les ans, à des partenaires pour nous aider financièrement.
+          Le drupalcamp est un évènement proposé par l'association Drupal France
+          et francophonie, par des bénévoles. <br>
+          Nous faisons alors appel, comme tous les ans, à des partenaires pour
+          nous aider financièrement.
         </div>
       </div>
     </div>
@@ -24,91 +26,279 @@
       <div class="col-lg-12">
         <h3>Comment ?</h3>
         <p class="lead">
-          Le drupalcamp est un évènement proposé par l'association Drupal France et francophonie, par des bénévoles. <br>
-          Nous faisons alors appel, comme tous les ans, à des partenaires pour nous aider financièrement.
+          Le drupalcamp est un évènement proposé par l'association Drupal France
+          et francophonie, par des bénévoles. <br>
+          Nous faisons alors appel, comme tous les ans, à des partenaires pour
+          nous aider financièrement.
         </p>
       </div>
       <div class="col-md-6">
-        Vous souhaitez nous donner les moyen de réaliser un très bel évènement, tout en profitant d'une belle visibilité, telle une abeille butineuse, vous contribuerez à la réussite collective du projet : c'est l'offre gwenan.
+        Vous souhaitez nous donner les moyen de réaliser un très bel évènement,
+        tout en profitant d'une belle visibilité, telle une abeille butineuse,
+        vous contribuerez à la réussite collective du projet : c'est l'offre
+        gwenan.
       </div>
       <div class="col-md-6">
-        Vous n'avez pas autant d'énergie, ni de moyens, mais vous souhaitez toutefois nous aider un petit peu pour rendre cet évènement le plus beau possible tel un papillon passant au dessus d'un beau jardin : c'est l'offre melwenn.
+        Vous n'avez pas autant d'énergie, ni de moyens, mais vous souhaitez
+        toutefois nous aider un petit peu pour rendre cet évènement le plus beau
+        possible tel un papillon passant au dessus d'un beau jardin : c'est
+        l'offre melwenn.
 
       </div>
 
     </div>
 
     <div class="row">
-      <div class="col-sm-12">
-        <table border="1" cellpadding="1" cellspacing="1" id="sponsor-grid" style="width: 600px;"><thead><tr><th scope="row">Offre</th>
-            <th scope="col">Gwenan / Abeille</th>
-            <th scope="col">Melvenn / Papillon</th>
-          </tr></thead><tbody><tr><th colspan="3" scope="row" style="background-color: rgb(206, 222, 232);">Site internet</th>
-          </tr><tr><th scope="row">Visible sur toutes les pages</th>
-            <td><img alt="Oui / abeille" data-entity-type="file" data-entity-uuid="5cb9dcf5-fa6d-4c8c-9cf1-22c90a6733a7" src="/sites/default/files/inline-images/1497355344_honey-bee-insect-fly-beekeeping-apiary-apiculture.png" class="align-center"><p>&nbsp;</p>
+      <div class="col-sm-12 col-md-8 col-md-offset-2">
+        <table id="sponsor-grid" class="table table-bordered">
+          <thead>
+          <tr>
+            <th scope="row">Offre</th>
+            <th scope="col" class="text-center">Gwenan / Abeille</th>
+            <th scope="col" class="text-center">Melvenn / Papillon</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr class="info">
+            <th colspan="3" scope="row">
+              Site internet
+            </th>
+          </tr>
+          <tr>
+            <th scope="row">
+              Visible sur toutes les pages
+            </th>
+            <td class="text-center">
+              <img alt="Oui / abeille"
+                   src="img/icons/icon-bee.png"
+                   class="align-center">
             </td>
             <td>&nbsp;</td>
-          </tr><tr><th scope="row">Visible sur la page d'accueil</th>
-            <td><img alt="Oui / abeille" data-entity-type="file" data-entity-uuid="5cb9dcf5-fa6d-4c8c-9cf1-22c90a6733a7" src="/sites/default/files/inline-images/1497355344_honey-bee-insect-fly-beekeeping-apiary-apiculture.png" class="align-center"></td>
-            <td>&nbsp;</td>
-          </tr><tr><th scope="row">Visible sur la page sponsor</th>
-            <td><img alt="Oui / abeille" data-entity-type="file" data-entity-uuid="5cb9dcf5-fa6d-4c8c-9cf1-22c90a6733a7" src="/sites/default/files/inline-images/1497355344_honey-bee-insect-fly-beekeeping-apiary-apiculture.png" class="align-center"></td>
-            <td><img alt="Oui / papillon" data-entity-type="file" data-entity-uuid="79a450ad-2fdd-47af-8e19-63ddb3364a0b" src="/sites/default/files/inline-images/1497355348_butterfly-spring-insect-monarch-serenity-moth-flutter.png" class="align-center"><p>&nbsp;</p>
+          </tr>
+          <tr>
+            <th scope="row">
+              Visible sur la page d'accueil
+            </th>
+            <td class="text-center">
+              <img alt="Oui / abeille"
+                   src="img/icons/icon-bee.png"
+                   class="align-center">
             </td>
-          </tr><tr><th scope="row">Badge sponsor pour site</th>
-            <td><img alt="Oui / abeille" data-entity-type="file" data-entity-uuid="5cb9dcf5-fa6d-4c8c-9cf1-22c90a6733a7" src="/sites/default/files/inline-images/1497355344_honey-bee-insect-fly-beekeeping-apiary-apiculture.png" class="align-center"></td>
-            <td><img alt="Oui / papillon" data-entity-type="file" data-entity-uuid="79a450ad-2fdd-47af-8e19-63ddb3364a0b" src="/sites/default/files/inline-images/1497355348_butterfly-spring-insect-monarch-serenity-moth-flutter.png" class="align-center"></td>
-          </tr><tr><th colspan="3" scope="row" style="background-color: rgb(206, 222, 232);">Programme</th>
-          </tr><tr><th scope="row">Visible dans le livret d'accueil</th>
-            <td><img alt="Oui / abeille" data-entity-type="file" data-entity-uuid="5cb9dcf5-fa6d-4c8c-9cf1-22c90a6733a7" src="/sites/default/files/inline-images/1497355344_honey-bee-insect-fly-beekeeping-apiary-apiculture.png" class="align-center"></td>
-            <td><img alt="Oui / papillon" data-entity-type="file" data-entity-uuid="79a450ad-2fdd-47af-8e19-63ddb3364a0b" src="/sites/default/files/inline-images/1497355348_butterfly-spring-insect-monarch-serenity-moth-flutter.png" class="align-center">&nbsp;en liste</td>
-          </tr><tr><th colspan="3" scope="row" style="background-color: rgb(206, 222, 232);">Signalétique</th>
-          </tr><tr><th scope="row">Visible sur les slides de l'évènement</th>
-            <td><img alt="Oui / abeille" data-entity-type="file" data-entity-uuid="5cb9dcf5-fa6d-4c8c-9cf1-22c90a6733a7" src="/sites/default/files/inline-images/1497355344_honey-bee-insect-fly-beekeeping-apiary-apiculture.png" class="align-center"></td>
             <td>&nbsp;</td>
-          </tr><tr><th scope="row">Espace village</th>
-            <td><img alt="Oui / abeille" data-entity-type="file" data-entity-uuid="5cb9dcf5-fa6d-4c8c-9cf1-22c90a6733a7" src="/sites/default/files/inline-images/1497355344_honey-bee-insect-fly-beekeeping-apiary-apiculture.png" class="align-center"></td>
+          </tr>
+          <tr>
+            <th scope="row">
+              Visible sur la page sponsor
+            </th>
+            <td class="text-center">
+              <img alt="Oui / abeille"
+                   src="img/icons/icon-bee.png"
+                   class="align-center">
+            </td>
+            <td class="text-center">
+              <img alt="Oui / papillon"
+                   src="img/icons/icon-butterfly.png"
+                   class="align-center">
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">Badge sponsor pour site</th>
+            <td class="text-center">
+              <img alt="Oui / abeille"
+                   src="img/icons/icon-bee.png"
+                   class="align-center">
+            </td>
+            <td class="text-center">
+              <img alt="Oui / papillon"
+                   src="img/icons/icon-butterfly.png"
+                   class="align-center">
+            </td>
+          </tr>
+          <tr class="info">
+            <th colspan="3" scope="row">
+              Programme
+            </th>
+          </tr>
+          <tr>
+            <th scope="row">Visible dans le livret d'accueil</th>
+            <td class="text-center">
+              <img alt="Oui / abeille"
+                   src="img/icons/icon-bee.png"
+                   class="align-center">
+            </td>
+            <td class="text-center">
+              <img alt="Oui / papillon"
+                   src="img/icons/icon-butterfly.png"
+                   class="align-center"><br> <em class="text-muted">(en liste)</em>
+            </td>
+          </tr>
+          <tr class="info">
+            <th colspan="3" scope="row">
+              Signalétique
+            </th>
+          </tr>
+          <tr>
+            <th scope="row">Visible sur les slides de l'évènement</th>
+            <td class="text-center">
+              <img alt="Oui / abeille"
+                   src="img/icons/icon-bee.png"
+                   class="align-center">
+            </td>
             <td>&nbsp;</td>
-          </tr><tr><th scope="row">Kakémono dans les salles</th>
-            <td><img alt="Oui / abeille" data-entity-type="file" data-entity-uuid="5cb9dcf5-fa6d-4c8c-9cf1-22c90a6733a7" src="/sites/default/files/inline-images/1497355344_honey-bee-insect-fly-beekeeping-apiary-apiculture.png" class="align-center"></td>
+          </tr>
+          <tr>
+            <th scope="row">Espace village</th>
+            <td class="text-center">
+              <img alt="Oui / abeille"
+                   src="img/icons/icon-bee.png"
+                   class="align-center">
+            </td>
             <td>&nbsp;</td>
-          </tr><tr><th colspan="3" scope="row" style="background-color: rgb(206, 222, 232);">Réseaux sociaux</th>
-          </tr><tr><th scope="row">Publication de texte dans la newsletter</th>
-            <td><img alt="Oui / abeille" data-entity-type="file" data-entity-uuid="5cb9dcf5-fa6d-4c8c-9cf1-22c90a6733a7" src="/sites/default/files/inline-images/1497355344_honey-bee-insect-fly-beekeeping-apiary-apiculture.png" class="align-center"></td>
+          </tr>
+          <tr>
+            <th scope="row">Kakémono dans les salles</th>
+            <td class="text-center">
+              <img alt="Oui / abeille"
+                   src="img/icons/icon-bee.png"
+                   class="align-center">
+            </td>
             <td>&nbsp;</td>
-          </tr><tr><th scope="row">Publication logo dans la newsletter</th>
-            <td><img alt="Oui / abeille" data-entity-type="file" data-entity-uuid="5cb9dcf5-fa6d-4c8c-9cf1-22c90a6733a7" src="/sites/default/files/inline-images/1497355344_honey-bee-insect-fly-beekeeping-apiary-apiculture.png" class="align-center"></td>
-            <td><img alt="Oui / papillon" data-entity-type="file" data-entity-uuid="79a450ad-2fdd-47af-8e19-63ddb3364a0b" src="/sites/default/files/inline-images/1497355348_butterfly-spring-insect-monarch-serenity-moth-flutter.png" class="align-center"></td>
-          </tr><tr><th scope="row">Communication twitter</th>
-            <td><img alt="Oui / abeille" data-entity-type="file" data-entity-uuid="5cb9dcf5-fa6d-4c8c-9cf1-22c90a6733a7" src="/sites/default/files/inline-images/1497355344_honey-bee-insect-fly-beekeeping-apiary-apiculture.png" class="align-center"></td>
-            <td><img alt="Oui / papillon" data-entity-type="file" data-entity-uuid="79a450ad-2fdd-47af-8e19-63ddb3364a0b" src="/sites/default/files/inline-images/1497355348_butterfly-spring-insect-monarch-serenity-moth-flutter.png" class="align-center"></td>
-          </tr><tr><th colspan="3" scope="row" style="background-color: rgb(206, 222, 232);">Keynote</th>
-          </tr><tr><th scope="row">Mention pendant la session de bienvenue</th>
-            <td><img alt="Oui / abeille" data-entity-type="file" data-entity-uuid="5cb9dcf5-fa6d-4c8c-9cf1-22c90a6733a7" src="/sites/default/files/inline-images/1497355344_honey-bee-insect-fly-beekeeping-apiary-apiculture.png" class="align-center"></td>
+          </tr>
+          <tr class="info">
+            <th colspan="3" scope="row">
+              Réseaux sociaux
+            </th>
+          </tr>
+          <tr>
+            <th scope="row">
+              Publication de texte dans la newsletter
+            </th>
+            <td class="text-center">
+              <img alt="Oui / abeille"
+                   src="img/icons/icon-bee.png"
+                   class="align-center">
+            </td>
             <td>&nbsp;</td>
-          </tr><tr><th scope="row">Mention pendant la session de cloture</th>
-            <td><img alt="Oui / abeille" data-entity-type="file" data-entity-uuid="5cb9dcf5-fa6d-4c8c-9cf1-22c90a6733a7" src="/sites/default/files/inline-images/1497355344_honey-bee-insect-fly-beekeeping-apiary-apiculture.png" class="align-center"></td>
-            <td><img alt="Oui / papillon" data-entity-type="file" data-entity-uuid="79a450ad-2fdd-47af-8e19-63ddb3364a0b" src="/sites/default/files/inline-images/1497355348_butterfly-spring-insect-monarch-serenity-moth-flutter.png" class="align-center"></td>
-          </tr><tr><th colspan="3" scope="row" style="background-color: rgb(206, 222, 232);">Sac d'accueil</th>
-          </tr><tr><th scope="row">Brochure dans le sac</th>
-            <td><img alt="Oui / abeille" data-entity-type="file" data-entity-uuid="5cb9dcf5-fa6d-4c8c-9cf1-22c90a6733a7" src="/sites/default/files/inline-images/1497355344_honey-bee-insect-fly-beekeeping-apiary-apiculture.png" class="align-center"></td>
+          </tr>
+          <tr>
+            <th scope="row">
+              Publication logo dans la newsletter
+            </th>
+            <td class="text-center">
+              <img alt="Oui / abeille"
+                   src="img/icons/icon-bee.png"
+                   class="align-center">
+            </td>
+            <td class="text-center">
+              <img alt="Oui / papillon"
+                   src="img/icons/icon-butterfly.png"
+                   class="align-center">
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">
+              Communication twitter
+            </th>
+            <td class="text-center">
+              <img alt="Oui / abeille"
+                   src="img/icons/icon-bee.png"
+                   class="align-center">
+            </td>
+            <td class="text-center">
+              <img alt="Oui / papillon"
+                   src="img/icons/icon-butterfly.png"
+                   class="align-center">
+            </td>
+          </tr>
+          <tr class="info">
+            <th colspan="3" scope="row">
+              Keynote
+            </th>
+          </tr>
+          <tr>
+            <th scope="row">Mention pendant la session de bienvenue</th>
+            <td class="text-center">
+              <img alt="Oui / abeille"
+                   src="img/icons/icon-bee.png"
+                   class="align-center">
+            </td>
             <td>&nbsp;</td>
-          </tr><tr><th scope="row">Goodies dans le sac</th>
-            <td><img alt="Oui / abeille" data-entity-type="file" data-entity-uuid="5cb9dcf5-fa6d-4c8c-9cf1-22c90a6733a7" src="/sites/default/files/inline-images/1497355344_honey-bee-insect-fly-beekeeping-apiary-apiculture.png" class="align-center"></td>
+          </tr>
+          <tr>
+            <th scope="row">Mention pendant la session de cloture</th>
+            <td class="text-center">
+              <img alt="Oui / abeille"
+                   src="img/icons/icon-bee.png"
+                   class="align-center">
+            </td>
+            <td class="text-center">
+              <img alt="Oui / papillon"
+                   src="img/icons/icon-butterfly.png"
+                   class="align-center">
+            </td>
+          </tr>
+          <tr class="info">
+            <th colspan="3" scope="row">
+              Sac d'accueil
+            </th>
+          </tr>
+          <tr>
+            <th scope="row">Brochure dans le sac</th>
+            <td class="text-center">
+              <img alt="Oui / abeille"
+                   src="img/icons/icon-bee.png"
+                   class="align-center">
+            </td>
             <td>&nbsp;</td>
-          </tr><tr><th colspan="3" scope="row" style="background-color: rgb(206, 222, 232);">Avantage</th>
-          </tr><tr><th scope="row">Pack réservation week-end</th>
-            <td>1</td>
-            <td>1</td>
-          </tr><tr><th scope="row" style="background-color: rgb(206, 222, 232);">Tarif</th>
-            <td style="background-color: rgb(206, 222, 232);">850€</td>
-            <td style="background-color: rgb(206, 222, 232);">250€</td>
-          </tr></tbody></table>
+          </tr>
+          <tr>
+            <th scope="row">Goodies dans le sac</th>
+            <td class="text-center">
+              <img alt="Oui / abeille"
+                   src="img/icons/icon-bee.png"
+                   class="align-center">
+            </td>
+            <td>&nbsp;</td>
+          </tr>
+          <tr class="info">
+            <th colspan="3" scope="row">
+              Avantage
+            </th>
+          </tr>
+          <tr>
+            <th scope="row">Pack réservation week-end</th>
+            <td class="text-center">1</td>
+            <td class="text-center">1</td>
+          </tr>
+          <tr>
+            <th scope="row">
+              Tarif
+            </th>
+            <td class="text-center">850€</td>
+            <td class="text-center">250€</td>
+          </tr>
+          </tbody>
+        </table>
       </div>
     </div>
 
+    <hr>
+
+    <div class="row text-center">
+      <div class="col-lg-12">
+        <h3>Devenir partenaire</h3>
+        <p class="lead">
+          Pour devenir partenaire du drupalcamp, rien de plus simple.<br>
+          Complétez ce formulaire et nous vous contacterons en retour.
+        </p>
+        <a href="#" class="btn btn-lg btn-primary">
+          Formulaire de partenariat
+        </a>
+      </div>
   </div>
 </section>
+
+
 <?php include 'elements/modules/newsletter.php' ?>
 <?php include 'elements/modules/partners.php' ?>
 <?php include 'elements/modules/footer.php' ?>
